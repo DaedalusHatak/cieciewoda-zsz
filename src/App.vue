eb71347ac6fe5bc39806cec69529c14a4c285cdc
DaedalusHatak <pawelkic55@gmail.com>
2022-10-25 12:00:43 +0200

    Vuetifyworks

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,47 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+	<v-app>
+		
+		<v-app-bar app>
+      
+			<v-app-bar-nav-icon class="hidden-sm-and-up" variant="text" @click.stop="drawer = !drawer">
+			</v-app-bar-nav-icon>
+			<v-toolbar-title>Coding Beauty</v-toolbar-title>
+			<v-toolbar-items class="hidden-xs">
+				<v-btn flat v-for="items in items" :key="items.title" :to="items.link">{{
+					items.title
+				}}</v-btn>
+			</v-toolbar-items>
+		</v-app-bar>
+
+
+
+    <v-navigation-drawer  class="hidden-print-only" disable-route-watcher disable-resize-watcher  v-model="drawer" bottom temporary>
+			<v-list>
+        <v-btn size="x-large" :density="comfortable" flat block nav v-for="items in items" :key="items.title" :to="items.link">{{
+					items.title
+				}}</v-btn>
+      </v-list>
+		</v-navigation-drawer>
+		<!-- Sizes your content based upon application components -->
+		<v-main>
+			<!-- Provides the application the proper gutter -->
+			<v-container fluid>
+				<!-- If using vue-router -->
+				<router-view></router-view>
+			</v-container>
+		</v-main>
+
+		<v-footer app>
+			<!-- -->
+		</v-footer>
+	</v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script setup>
+import { ref } from 'vue';
+const drawer = ref('');
+const items = [
+	{ title: 'Home', value: 'home', link:"/"},
+	{ title: 'About', value: 'about', link:"/about"},
+];
+</script>
