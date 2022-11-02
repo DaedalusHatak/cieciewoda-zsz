14bf50234c241d95333f5cd263667d9ef8b7d654
DaedalusHatak <pawelkic55@gmail.com>
2022-11-02 12:38:39 +0100

    Usługi

--- src/views/UslugiView.vue
+++ src/views/UslugiView.vue
@@ -1,12 +1,27 @@
 <template>
-  <v-container class="bg-aqua">
+	<v-container class="bg-aqua">
 		<HeaderBackground v-bind:heading="start"></HeaderBackground
-		></v-container
-	>
-
-
+	></v-container>
+	<v-container>
+		<p class="text-h4 pa-12"
+			>Nasza firma świadcząca fachowe usługi „abrasive waterjet” - czyli cięcie
+			strumieniem wody Gwarantuje pełen profesjonalizm, krótki czas realizacji
+			zleceń oraz konkurencyjne ceny!!!</p
+		>
+	</v-container>
+	<v-container class="dark">
+		<TablePrices></TablePrices>
+	</v-container>
 </template>
 <script setup>
 import HeaderBackground from '../components/HeaderBackground.vue';
-const start = "Usługi";
+import TablePrices from '../components/TablePrices.vue'
+
+const start = 'Usługi';
 </script>
+
+<style>
+.dark{
+	background-color: #212121;
+}
+</style>
