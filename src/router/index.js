c17042e7c6405a26563ed42efe0b099d9c54d2f7
DaedalusHatak <pawelkic55@gmail.com>
2022-11-04 09:55:50 +0100

    scrollBehavior

--- src/router/index.js
+++ src/router/index.js
@@ -28,7 +28,11 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    // always scroll to top
+    return { top: 0 }
+  },
 })
 
 export default router
