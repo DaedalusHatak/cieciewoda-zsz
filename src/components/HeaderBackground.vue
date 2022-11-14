68bbb0e7f2a59a0ba1055dfcc8b117d9c63613b9
DaedalusHatak <pawelkic55@gmail.com>
2022-11-14 20:01:21 +0100

    Hero working everywhere

--- src/components/HeaderBackground.vue
+++ src/components/HeaderBackground.vue
@@ -1,46 +1,39 @@
 <template>
-  
-
-    <div class="d-flex flex-column fill-height block align-center background justify-center text-white">
-      <h1 class="text-xs-h6 text-md-h3 text-lg-h2">{{props.heading}}</h1>
-      <slot> 
-      </slot>
+  <div
+    class="d-flex flex-column fill-height block align-center background justify-center text-white"
+  >
+    <h1 class="text-xs-h6 text-md-h3 text-lg-h2">{{ props.heading }}</h1>
+    <slot> </slot>
   </div>
-
-    
-  </template>
-  <script setup>
-  import { defineProps } from 'vue';
+</template>
+<script setup>
+import { defineProps } from "vue";
 
 const props = defineProps({
-	heading: {
-		type: String,
-	},
-  apex:{
+  heading: {
     type: String,
-  }
+  },
 });
-  </script>
-  <style>
-  .v-container {
-    padding: 0;
-    margin: 0;
-    max-width: 100%;
-  }
-  
-  .bg-aqua {
-    background-color: aqua;
-  }
-  .background{
-    background-image: url("../assets/background-dark-top.jpg");
-    background-attachment: fixed;
-    position: relative;
-    background-position:top;
-    min-height: 400px;
-  }
-  img {
-    object-fit: contain;
-    height: 100%;
-    width: 100%;
-  }
-  </style>
+</script>
+<style>
+.v-container {
+  padding: 0;
+  margin: 0;
+  max-width: 100%;
+}
+.bg-aqua {
+  background-color: aqua;
+}
+.background {
+  background-image: url("../assets/background-dark-top.jpg");
+  background-attachment: fixed;
+  position: relative;
+  background-position: top;
+  min-height: 400px;
+}
+img {
+  object-fit: contain;
+  height: 100%;
+  width: 100%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-	<v-container class="bgg" height="auto"  center>
+	<v-container class="bgg"  center>
 		<HeaderBackground v-bind:apex="height" v-bind:heading="start">
 			<p class="text-h6 mt-16 pt-16">
 				Cięcie materiału metodą Abrasive WaterJet(AWJ).
@@ -24,17 +24,18 @@ import HeaderBackground from '../components/HeaderBackground.vue';
 import CarouselImage from '../components/CarouselImage.vue';
 const start = 'Profesjonalne cięcie wodą!';
 
-
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
 </script>
 <style lang="scss" scoped>
 .bg-dark {
 	color: white;
 	background-color: #212121;
 }
+
 .background {	
-	
-	min-height: 100vh;
-	height: 100%;
+	min-height: calc(var(--vh, 1vh) * 100);
 	h1 {
 		padding-bottom: 4em !important;
 	}
