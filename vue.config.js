9db2e588b92dbaa4c1a2f85db13528b5d857c86f
DaedalusHatak <pawelkic55@gmail.com>
2022-11-17 10:52:25 +0100

    pwa iconPath

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  pwa: {
+    iconPaths: {
+       favicon32: 'img/icons/wer.ico',
+       favicon16: 'img/icons/wer.ico',
+       favicon: 'img/icons/wer.ico'
+    }
+  },
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
